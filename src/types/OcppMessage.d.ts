22a946df9275b593790170d271cf838c384318db
Fabian Stern <fabian.stern@visia-it.com>
2022-07-30 23:00:48 +0200

    Add InboundOcppMessage, OutboundOcppMessage, RespondableOcppMessage, ResultingOcppMessage & related type definitions

--- src/types/OcppMessage.d.ts
+++ src/types/OcppMessage.d.ts
@@ -1,3 +1,5 @@
+import OcppClient from './OcppClient';
+
 declare enum OcppMessageType {
   CALL = 2,
   CALLRESULT = 3,
@@ -10,5 +12,41 @@ declare type OcppMessage = {
   type: OcppMessageType;
   id: OcppMessageId;
 };
+
+declare type InboundOcppMessage = OcppMessage & {
+  from: OcppClient;
+};
+
+declare type OutboundOcppMessage = OcppMessage & {
+  to: OcppClient;
+  get state(): OutboundOcppMessageState;
+};
+
+declare enum OutboundOcppMessageState {
+  Unsent,
+  Sent,
+}
+
+declare type RespondableOcppMessage<TResponse extends OutboundOcppMessage> =
+  InboundOcppMessage & {
+    respond: (response: TResponse) => void;
+    get state(): RespondableOcppMessageState;
+  };
+
+declare enum RespondableOcppMessageState {
+  ResponseUnsent,
+  ResponseSent,
+}
+
+declare type ResultingOcppMessage<TResponse extends InboundOcppMessage> =
+  OutboundOcppMessage & {
+    handleResponse: (response: TResponse) => void;
+    get state(): ResultingOcppMessageState;
+  };
+
+declare enum ResultingOcppMessageState {
+  ResponsePending,
+  ResponseReceived,
+}
 
 export default OcppMessage;
