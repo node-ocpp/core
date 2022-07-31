4753a597e1854cd2ce7afd329c7cbe6bb0911c57
Fabian Stern <fabian.stern@visia-it.com>
2022-08-01 00:11:44 +0200

    Add partial OcppEndpoint type definition & implement base methods

---
+++ src/common/OcppEndpoint.ts
@@ -0,0 +1,140 @@
+import OcppClient from './OcppClient';
+import OcppSession from './OcppSession';
+import {
+  InboundOcppMessage,
+  OutboundOcppMessage,
+} from '../types/ocpp/OcppMessage';
+
+abstract class OcppEndpoint<
+  TConfig extends OcppEndpointConfig,
+  TClient extends OcppClient,
+  TSession extends OcppSession<TClient>,
+  TInboundMessage extends InboundOcppMessage,
+  TOutboundMessage extends OutboundOcppMessage,
+  TAuthenticationProperties extends OcppAuthenticationProperties<
+    TClient,
+    TSession
+  >,
+  TAuthenticationHandler extends OcppAuthenticationHandler<
+    TClient,
+    TSession,
+    TAuthenticationProperties
+  >
+> {
+  public readonly config: TConfig;
+
+  private sessions: TSession[];
+  private authenticationHandlers: TAuthenticationHandler[];
+  private messageHandlers: OcppMessageHandler[];
+
+  protected abstract get isListening(): boolean;
+
+  protected abstract handleCreate(): Promise<void>;
+  protected abstract handleListen(): Promise<void>;
+  protected abstract handleStop(): Promise<void>;
+  protected abstract handleOutboundMessage(
+    message: TOutboundMessage
+  ): Promise<TInboundMessage | void>;
+
+  constructor(
+    config: TConfig,
+    authenticationHandlers: TAuthenticationHandler[],
+    messageHandlers: OcppMessageHandler[]
+  ) {
+    this.config = config;
+    this.authenticationHandlers = authenticationHandlers;
+    this.messageHandlers = messageHandlers;
+    this.sessions = new Array<TSession>();
+    this.handleCreate();
+  }
+
+  public async listen() {
+    if (this.isListening) {
+      throw new Error('Endpoint is already listening for connections');
+    }
+
+    await this.handleListen();
+  }
+
+  public async stop() {
+    if (!this.isListening) {
+      throw new Error('Endpoint is currently not listening for connections');
+    }
+
+    await this.handleStop();
+  }
+
+  public async sendMessage(message: TOutboundMessage) {
+    if (!this.isListening) {
+      throw new Error('Endpoint is currently not listening for connections');
+    } else if (!this.getSession(message.recipient.id)) {
+      throw new Error(
+        `Client with id ${message.recipient} is currently not connected`
+      );
+    }
+
+    return await this.handleOutboundMessage(message);
+  }
+
+  public getSession(clientId: string): TSession | false {
+    const session = this.sessions.find(
+      _session => _session.client.id === clientId
+    );
+
+    return session || false;
+  }
+
+  protected onClientConnected(session: TSession) {
+    if (this.getSession(session.client.id)) {
+      throw new Error(
+        `Client with id ${session.client.id} is already connected`
+      );
+    }
+
+    this.sessions.push();
+  }
+
+  protected onClientDisconnected(session: TSession) {
+    if (!this.getSession(session.client.id)) {
+      throw new Error(
+        `Client with id ${session.client.id} is currently not connected`
+      );
+    }
+
+    this.sessions = this.sessions.filter(
+      _session => _session.client.id !== session.client.id
+    );
+  }
+
+  protected onInboundMessage(message: TInboundMessage) {
+    return; // TODO
+  }
+}
+
+type OcppEndpointConfig = {
+  port: number;
+};
+
+interface OcppAuthenticationHandler<
+  TClient extends OcppClient,
+  TSession extends OcppSession<TClient>,
+  TAuthenticationProperties extends OcppAuthenticationProperties<
+    TClient,
+    TSession
+  >
+> {
+  handleAuthentication: (
+    properties: TAuthenticationProperties,
+    next: () => void
+  ) => TSession;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface OcppAuthenticationProperties<
+  TClient extends OcppClient,
+  TSession extends OcppSession<TClient>
+> {}
+
+interface OcppMessageHandler {
+  handleMessage: (message: InboundOcppMessage, next: () => void) => void;
+}
