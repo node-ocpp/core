13d646c407935d00daead34a1ffea91281938e6d
Fabian Stern <fabian.stern@visia-it.com>
2022-07-31 05:48:37 +0200

    Add BootNotificationRequest & BootNotificationResponse message types

---
+++ src/types/ocpp/messages/BootNotification.d.ts
@@ -0,0 +1,33 @@
+import { InboundOcppCall } from '../OcppCallMessage';
+import { OutboundOcppCallResult } from '../OcppCallResultMessage';
+
+declare type BootNotificationRequest = InboundOcppCall<
+  BootNotificationRequestPayload,
+  BootNotificationResponsePayload,
+  BootNotificationResponse
+>;
+
+declare type BootNotificationRequestPayload = {
+  chargeBoxSerialNumber?: string;
+  chargePointModel: string;
+  chargePointSerialNumber?: string;
+  chargePointVendor: string;
+  firmwareVersion?: string;
+  iccid?: string;
+  imsi?: string;
+  meterSerialNumber?: string;
+  meterType?: string;
+};
+
+declare type BootNotificationResponse =
+  OutboundOcppCallResult<BootNotificationResponsePayload>;
+
+declare type BootNotificationResponsePayload = {
+  currentTime: Date;
+  interval: number;
+  status: RegistrationStatus;
+};
+
+declare type RegistrationStatus = 'Accepted' | 'Pending' | 'Rejected';
+
+export { BootNotificationRequest, BootNotificationResponse };
