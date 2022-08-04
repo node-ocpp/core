64f02dd8599380f2ae08847f5327d624d54caf0e
Fabian Stern <fabian.stern@visia-it.com>
2022-08-04 20:11:19 +0200

    Add getters/setters for is isActive, client & pendingMessage properties of OcppSession

--- src/common/OcppSession.ts
+++ src/common/OcppSession.ts
@@ -1,13 +1,27 @@
+import { InboundOcppMessage } from '../types/ocpp/OcppMessage';
 import OcppClient from './OcppClient';
 
 abstract class OcppSession<TClient extends OcppClient> {
+  abstract get isActive(): boolean;
+
   private _client: TClient;
+  private _pendingMessage!: InboundOcppMessage;
 
   constructor(client: TClient) {
     this._client = client;
   }
 
-  abstract get client(): TClient;
+  get client(): TClient {
+    return this._client;
+  }
+
+  get pendingMessage() {
+    return this._pendingMessage;
+  }
+
+  set pendingMessage(message: InboundOcppMessage) {
+    this._pendingMessage = message;
+  }
 }
 
 export default OcppSession;
