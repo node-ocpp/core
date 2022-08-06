67245a561febf6c2f6b87ed83f28ec2eeb403f85
Fabian Stern <fabian.stern@visia-it.com>
2022-08-07 00:22:34 +0200

    Add UnlockConnectorRequest & UnlockConnectorResponse type definitions

---
+++ src/types/messages/UnlockConnector.d.ts
@@ -0,0 +1,22 @@
+import { OutboundOcppCall } from '../../common/OcppCallMessage';
+import { InboundOcppCallResult } from '../../common/OcppCallResultMessage';
+
+declare type UnlockConnectorRequest = OutboundOcppCall<
+  UnlockConnectorRequestPayload,
+  UnlockConnectorResponsePayload,
+  UnlockConnectorResponse
+>;
+
+declare type UnlockConnectorRequestPayload = {
+  connectorId: number;
+};
+
+declare type UnlockConnectorResponse = InboundOcppCallResult<UnlockConnectorResponsePayload>;
+
+declare type UnlockConnectorResponsePayload = {
+  status: UnlockStatus;
+};
+
+declare type UnlockStatus = 'Unlocked' | 'UnlockFailed' | 'NotSupported';
+
+export { UnlockConnectorRequest, UnlockConnectorResponse };
