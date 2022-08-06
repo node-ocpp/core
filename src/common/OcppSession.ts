c91772cc3411d139ac8d50c92580a4d367eb2590
Fabian Stern <fabian.stern@visia-it.com>
2022-08-07 00:29:05 +0200

    Add protocol property to OcppSession

--- src/common/OcppSession.ts
+++ src/common/OcppSession.ts
@@ -5,14 +5,20 @@ abstract class OcppSession<TClient extends OcppClient> {
   abstract get isActive(): boolean;
 
   private _client: TClient;
+  private _protocol: OcppProtocolVersion;
   private _pendingMessage!: OcppMessage;
 
-  constructor(client: TClient) {
+  constructor(client: TClient, protocol: OcppProtocolVersion) {
     this._client = client;
+    this._protocol = protocol;
   }
 
-  get client(): TClient {
+  get client() {
     return this._client;
+  }
+
+  get protocol() {
+    return this._protocol;
   }
 
   get pendingMessage() {
@@ -23,5 +29,7 @@ abstract class OcppSession<TClient extends OcppClient> {
     this._pendingMessage = message;
   }
 }
+
+type OcppProtocolVersion = 'ocpp1.5' | 'ocpp1.6' | 'ocpp2.0' | 'ocpp2.0.1';
 
 export default OcppSession;
