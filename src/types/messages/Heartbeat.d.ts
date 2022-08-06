2059023ea80070537db9f3174052f6e639d9b305
Fabian Stern <fabian.stern@visia-it.com>
2022-08-06 22:12:22 +0200

    Add HeartbeatRequest & HeartbeatResponse type definitions

---
+++ src/types/messages/Heartbeat.d.ts
@@ -0,0 +1,12 @@
+import { InboundOcppCall } from '../../common/OcppCallMessage';
+import { OutboundOcppCallResult } from '../../common/OcppCallResultMessage';
+
+declare type HeartbeatRequest = InboundOcppCall<null, HeartbeatResponsePayload, HeartbeatResponse>;
+
+declare type HeartbeatResponse = OutboundOcppCallResult<HeartbeatResponsePayload>;
+
+declare type HeartbeatResponsePayload = {
+  currentTime: Date;
+};
+
+export { HeartbeatRequest, HeartbeatResponse };
