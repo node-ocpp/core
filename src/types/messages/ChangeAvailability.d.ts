d84253ac29de9513430bf404e430f8e82d31d95c
Fabian Stern <fabian.stern@visia-it.com>
2022-08-06 19:04:34 +0200

    Add ChangeAvailabilityRequest & ChangeAvailabilityResponse type definitions

---
+++ src/types/messages/ChangeAvailability.d.ts
@@ -0,0 +1,25 @@
+import { OutboundOcppCall } from '../../common/OcppCallMessage';
+import { InboundOcppCallResult } from '../../common/OcppCallResultMessage';
+
+declare type ChangeAvailabilityRequest = OutboundOcppCall<
+  ChangeAvailabilityRequestPayload,
+  ChangeAvailabilityResponsePayload,
+  ChangeAvailabilityResponse
+>;
+
+declare type ChangeAvailabilityRequestPayload = {
+  connectorId: number;
+  type: AvailabilityType;
+};
+
+declare type AvailabilityType = 'Inoperative' | 'Operative';
+
+declare type ChangeAvailabilityResponse = InboundOcppCallResult<ChangeAvailabilityResponsePayload>;
+
+declare type ChangeAvailabilityResponsePayload = {
+  status: AvailabilityStatus;
+};
+
+declare type AvailabilityStatus = 'Accepted' | 'Rejected';
+
+export { ChangeAvailabilityRequest, ChangeAvailabilityResponse };
