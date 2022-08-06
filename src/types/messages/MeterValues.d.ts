e42164fb4256b83fa08e3150cbe447f02a430f33
Fabian Stern <fabian.stern@visia-it.com>
2022-08-06 19:45:58 +0200

    Add type definitions for MeterValuesRequest & MeterValuesResponse

---
+++ src/types/messages/MeterValues.d.ts
@@ -0,0 +1,101 @@
+import { InboundOcppCall } from '../../common/OcppCallMessage';
+import { OutboundOcppCallResult } from '../../common/OcppCallResultMessage';
+
+declare type MeterValuesRequest = InboundOcppCall<
+  MeterValuesRequestPayload,
+  null,
+  MeterValuesResponse
+>;
+
+declare type MeterValuesResponse = OutboundOcppCallResult<null>;
+
+declare type MeterValuesRequestPayload = {
+  connectorId: number;
+  transactionId?: number;
+  meterValues: MeterValue[];
+};
+
+declare type MeterValue = {
+  timestamp: Date;
+  sampledValue: SampledValue[];
+};
+
+declare type SampledValue = {
+  value: string;
+  context?: ReadingContext;
+  format?: ValueFormat;
+  measurand?: Measurand;
+  phase?: Phase;
+  location?: Location;
+  unit?: UnitOfMeasure;
+};
+
+declare type ReadingContext =
+  | 'Interruption.Begin'
+  | 'Interruption.End'
+  | 'Other'
+  | 'Sample.Clock'
+  | 'Sample.Periodic'
+  | 'Transaction.Begin'
+  | 'Transaction.End'
+  | 'Trigger';
+
+declare type ValueFormat = 'Raw' | 'SignedData';
+
+declare type Measurand =
+  | 'Current.Export'
+  | 'Current.Import'
+  | 'Current.Offered'
+  | 'Energy.Active.Export.Register'
+  | 'Energy.Active.Import.Register'
+  | 'Energy.Reactive.Export.Register'
+  | 'Energy.Reactive.Import.Register'
+  | 'Energy.Active.Export.Interval'
+  | 'Energy.Active.Import.Interval'
+  | 'Energy.Reactive.Export.Interval'
+  | 'Energy.Reactive.Import.Interval'
+  | 'Frequency'
+  | 'Power.Active.Export'
+  | 'Power.Active.Import'
+  | 'Power.Factor'
+  | 'Power.Offered'
+  | 'Power.Reactive.Export'
+  | 'Power.Reactive.Import'
+  | 'RPM'
+  | 'SoC'
+  | 'Temperature'
+  | 'Voltage';
+
+declare type Phase =
+  | 'L1'
+  | 'L2'
+  | 'L3'
+  | 'N'
+  | 'L1-N'
+  | 'L2-N'
+  | 'L3-N'
+  | 'L1-L2'
+  | 'L2-L3'
+  | 'L3-L1';
+
+declare type Location = 'Body' | 'Cable' | 'EV' | 'Inlet' | 'Outlet';
+
+declare type UnitOfMeasure =
+  | 'Wh'
+  | 'kWh'
+  | 'varh'
+  | 'kvarh'
+  | 'W'
+  | 'kW'
+  | 'VA'
+  | 'kVA'
+  | 'var'
+  | 'kvar'
+  | 'A'
+  | 'V'
+  | 'Celsius'
+  | 'Fahrenheit'
+  | 'K'
+  | 'Percent';
+
+export { MeterValuesRequest, MeterValuesResponse };
