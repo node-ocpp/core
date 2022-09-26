d490224f1ed3bb1a48272c2c745c88645883f90e
Fabian Stern <fabian.stern@visia-it.com>
2022-09-26 12:15:18 +0200

    Allow AsyncHandler.handle() to return void

--- src/common/util/Handler.ts
+++ src/common/util/Handler.ts
@@ -5,7 +5,7 @@ abstract class AsyncHandler<TRequest> {
     this._next = handler;
   }
 
-  handle(request: TRequest): Promise<TRequest> {
+  handle(request: TRequest): Promise<TRequest | void> {
     if (this._next) {
       return this._next.handle(request);
     }
