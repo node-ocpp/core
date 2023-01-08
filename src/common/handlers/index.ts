765acefba8c5d1cb6d178b92754bcc2bd88280b7
Fabian Stern <fabian.stern@visia-it.com>
2023-01-08 17:57:57 +0100

    Rename pending-message.ts to session-properties.ts

--- src/common/handlers/index.ts
+++ src/common/handlers/index.ts
@@ -2,4 +2,4 @@ export { default as SessionExistsHandler } from './session-exists';
 
 export * from './message-actions';
 export * from './message-sync';
-export * from './pending-message';
+export * from './session-properties';
