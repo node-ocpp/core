888b2166306817bc4466c740fe52576daa6da7b8
Fabian Stern <fabian.stern@visia-it.com>
2023-01-08 17:55:54 +0100

    Rename actions-allowed.ts to message-actions.ts

--- src/common/handlers/index.ts
+++ src/common/handlers/index.ts
@@ -1,5 +1,5 @@
 export { default as SessionExistsHandler } from './session-exists';
 
-export * from './actions-allowed';
+export * from './message-actions';
 export * from './message-sync';
 export * from './pending-message';
