7e7e10a9a64f1315bcb473433700be39394a3d10
Fabian Stern <fabian.stern@visia-it.com>
2023-07-19 23:00:15 +0200

    Update ws endpoint example

--- examples/handlers/index.ts
+++ examples/handlers/index.ts
@@ -1,7 +1,6 @@
 export { default as BasicAuthHandler } from './basic-auth';
 
 export { default as AuthorizeHandler } from './Authorize';
-export { default as BootNotificationHandler } from './BootNotification';
 export { default as HeartbeatHandler } from './Heartbeat';
 export { default as MeterValuesHandler } from './MeterValues';
 export { default as StartTransactionHandler } from './StartTransaction';
